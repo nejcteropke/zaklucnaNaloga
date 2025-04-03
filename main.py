52c024bc59ae4bdf9bf307ecaf19502f114f7c4c
nejcteropke <hriskap4@gmail.com>
2025-04-03 09:48:16 +0000

    register

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'thisisasecretkey'
-class User(db.Model, UserMixin):
-    id = db.Column(db.integer, primary_key=True)
-    username = db.Column(db.string(30), nullable=False)
-    password = db.Column(db.string(90), nullable=False)
+
+
+
 
 @app.route('/')
 def index():
@@ -17,11 +11,16 @@ def index():
 
 @app.route('/login')
 def login():
+    
     return render_template('login.html')
 
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 
 
