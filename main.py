a04097b52bea74eceaf4d0c686bd61a608368de2
nejcteropke <hriskap4@gmail.com>
2025-04-09 08:14:47 +0000

    login

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+from tinydb import TinyDB, Query
 app = Flask(__name__)
 
-
+db = TinyDB('uporabniki.json')
+Uporabnik = Query()
 
 
 
@@ -16,6 +18,16 @@ def login():
 
 @app.route('/register')
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = db.get(Uporabnik.username == username)
+        if user:
+            return "Username already exists"
+        
+        db.insert({'username': username, 'password': password})
+        return "Successfully registered"
     return render_template('register.html')
 
 @app.route('/home')
